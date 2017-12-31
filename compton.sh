6c36651ea8f4a231e72798c71cea605062148992
Karan Dewan <karan@sigtuple.com>
2017-12-31 21:50:28 +0530

    fixed compton

---
+++ compton.sh
@@ -0,0 +1,13 @@
+
+
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q compton
+
+
+# Wait until the processes have been shut down
+while pgrep -x compton >/dev/null; do sleep 1; done
+
+# Launch polybar
+compton -CGb --config ~/.i3/compton.conf 
